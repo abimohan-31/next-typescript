59a61f8c43d511357e06b0761b1333b8eb40afa0
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-11-01 11:20:16 +0530

    buttons added with the styles

--- src/sections/user/view/user-list-view.tsx
+++ src/sections/user/view/user-list-view.tsx
@@ -4,7 +4,15 @@ export default function UserListView() {
   return (
     <div>
       <h1>User List View </h1>
-      <Button>Click</Button>
+      <Button>Click</Button>{""}
+      <Button variant="outline" size="lg">Click</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive" size="icon-lg">
+        Destructive
+      </Button>
+      <Button variant="link" size="lg">
+        Link
+      </Button>
     </div>
   );
 }
