7397946aa1031133e4f50eb244f1804d7f982cf4
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-11-01 11:06:37 +0530

    button installed through shadcn

--- src/sections/user/view/user-list-view.tsx
+++ src/sections/user/view/user-list-view.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function UserListView() {
   return (
     <div>
       <h1>User List View </h1>
+      <Button>Click</Button>
     </div>
   );
 }
